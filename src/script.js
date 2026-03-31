ce3c0e623652ad10e247083e29d8a3ba62d53acf
Triet Minh Nguyen <192999234+TrietMinhNguyen123@users.noreply.github.com>
2026-03-31 17:25:44 -0400

    Done day 3

--- src/script.js
+++ src/script.js
@@ -4,9 +4,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 500;
 
+let score = 0;
+let gameWon = false;
+const camera = {
+  x: 0,
+};
+
+const goal = {
+  x: 1200,
+  y: 380,
+  with: 40,
+  height: 70,
+};
+
+const coins = [
+  { x: 200, y: 225, size: 40, collected: false },
+  { x: 475, y: 325, size: 40, collected: false },
+  { x: 575, y: 125, size: 40, collected: false },
+];
 const player = {
-  x: 100,
-  y: 100,
+  x: 50,
+  y: 300,
   width: 50,
   height: 50,
 
@@ -19,6 +37,7 @@ const platforms = [
   { x: 100, y: 250, width: 250, height: 20 },
   { x: 400, y: 350, width: 150, height: 20 },
   { x: 500, y: 150, width: 150, height: 20 },
+  { x: 1000, y: 450, width: 300, height: 20 },
 ];
 const gravity = 0.5;
 
@@ -46,13 +65,48 @@ document.addEventListener("keyup", (e) => {
 function drawGround() {
   for (let platform of platforms) {
     ctx.fillStyle = "#540808";
-    ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
+    ctx.fillRect(
+      platform.x - camera.x,
+      platform.y,
+      platform.width,
+      platform.height,
+    );
   }
+}
+
+function drawCoins() {
+  for (coin of coins) {
+    if (!coin.collected) {
+      ctx.fillStyle = "gold";
+      ctx.beginPath();
+      ctx.arc(coin.x - camera.x, coin.y, coin.size / 2, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  }
+}
+
+function drawGoal() {
+  ctx.fillStyle = "cyan";
+  ctx.fillRect(goal.x - camera.x, goal.y, goal.with, goal.height);
 }
 
 function drawPlayer() {
   ctx.fillStyle = "green";
-  ctx.fillRect(player.x, player.y, player.width, player.height);
+  ctx.fillRect(player.x - camera.x, player.y, player.width, player.height);
+}
+
+function Win() {
+  if (gameWon) {
+    ctx.fillStyle = "black";
+    ctx.font = "40px Arial";
+    ctx.fillText("YOU WIN", 300, 200);
+  }
+}
+
+function showScore() {
+  ctx.fillStyle = "black";
+  ctx.font = "20px Arial";
+  ctx.fillText("Coins: " + score, 20, 30);
 }
 
 function updatePlayer() {
@@ -77,6 +131,9 @@ function updatePlayer() {
       player.x + player.width > platform.x &&
       player.x < platform.x + platform.width &&
       player.velocityY >= 0;
+    {
+      gameWon = false;
+    }
 
     if (isLanding) {
       player.y = platform.y - player.height;
@@ -84,15 +141,31 @@ function updatePlayer() {
       onGround = true;
     }
   }
+
+  for (let coin of coins) {
+    if (!coin.collected) {
+      const dx = player.x + player.width / 2 - coin.x;
+      const dy = player.y + player.height / 2 - coin.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < coin.size) {
+        coin.collected = true;
+        score++;
+        console.log(score);
+      }
+    }
+  }
 }
 
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   updatePlayer();
-
   drawPlayer();
+  drawGoal();
+  drawCoins();
   drawGround();
-
+  showScore();
+  camera.x = player.x - canvas.width / 2;
   requestAnimationFrame(gameLoop);
 }
 
