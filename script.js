c3f033230484a8ab66ada786c203c0b7e46a666a
driedclover <moistenclover@gmail.com>
2026-03-31 17:20:50 -0400

    finished day 1

--- script.js
+++ script.js
@@ -15,53 +15,12 @@ const player = {
     velocityY: 0,
 };
 
-function drawPlayer() {
-    ctx.fillStyle = "blue";
-    ctx.fillRect(player.x, player.y, player.width, player.height);
-}
-
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    updatePlayer();
-    drawGround();
-    drawPlayer();
-
-    requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
-
 const ground = {
     y: 450,
     height: 50
 };
 
-function drawGround() {
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, ground.y, canvas.width, ground.height);
-}
-
 const gravity = 0.5;
-
-function updatePlayer() {
-    if (keys.left) player.velocityX = -5;
-    if (keys.right) player.velocityX = 5;
-    else player.velocityX = 0;
-
-    player.x += player.velocityX;
-
-    player.velocityY += gravity;
-    player.y += player.velocityY;
-
-    if (player.y + player.height >= ground.y) {
-        player.y = ground.y - player.height;
-        player.velocityY = 0;
-        onGround = true;
-    } else {
-        onGround = false;
-    }
-}
 
 const keys = {
     left: false,
@@ -86,3 +45,46 @@ document.addEventListener("keydown", (e) => {
         player.velocityY = -12;
     }
 });
+
+function drawPlayer() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+
+function drawGround() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, ground.y, canvas.width, ground.height);
+}
+
+
+function updatePlayer() {
+    if (keys.left) player.velocityX = -5;
+    else if (keys.right) player.velocityX = 5;
+    else player.velocityX = 0;
+
+    player.x += player.velocityX;
+
+    player.velocityY += gravity;
+    player.y += player.velocityY;
+
+    if (player.y + player.height >= ground.y) {
+        player.y = ground.y - player.height;
+        player.velocityY = 0;
+        onGround = true;
+    } else {
+        onGround = false;
+    }
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    updatePlayer();
+    drawGround();
+    drawPlayer();
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
