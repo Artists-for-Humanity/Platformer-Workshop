51a7c512e0e9761c504d644858d1bcea6509f0e2
MrO3420 <omason632@gmail.com>
2026-04-01 18:44:09 -0400

    1 and 2 finished

--- script.js
+++ script.js
@@ -15,57 +15,100 @@ const player = {
     velocityY: 0,
 };
 
-
-function drawPlayer() {
-  ctx.fillStyle = "blue";
-  ctx.fillRect(player.x, player.y, player.width, player.height);
-}
-
-function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  drawPlayer();
-
-  requestAnimationFrame(gameLoop);
-}
-
-
-function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  drawGround();
-  drawPlayer();
-
-  requestAnimationFrame(gameLoop);
-}
-
 const ground = {
     y: 450,
     height: 50
 };
 
 
-function drawGround() {
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, ground.y, canvas.width, ground.height);
-}
-
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    drawGround();
-    drawPlayer();
-
-    requestAnimationFrame(gameLoop)
-}
-
-
 const gravity = 0.5;
 
-function updatePlayer() {
-    player.velocityY += gravity;
-    player.y += player.velocityY;
+
+
+
+
+function drawPlayer() {
+  ctx.fillStyle = "blue";
+  ctx.fillRect(player.x, player.y, player.width, player.height);
 }
+
+
+
+function drawGround() {
+  for (let platform of platforms) {
+    ctx.fillStyle = "green";
+    ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
+  }
+}
+
+const keys = {
+  left: false,
+  right: false,
+};
+
+document.addEventListener("keydown", (e) => {
+  console.log(e);
+  if (e.key === "a") keys.left = true;
+  if (e.key === "d") keys.right = true;
+
+    if (e.key === "w" && onGround) {
+    player.velocityY = -12;
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.key === "a") keys.left = false;
+  if (e.key === "d") keys.right = false;
+});
+
+
+
+function updatePlayer() {
+  if (keys.left) player.velocityX -= 0.5;
+  else if (keys.right) player.velocityX += 0.5;
+  else player.velocityX = 0;
+
+  player.velocityX *= 0.9;
+
+  player.x += player.velocityX;
+
+  player.velocityY += gravity;
+  player.y += player.velocityY;
+
+  if (player.y + player.height >= ground.y) {
+    player.y = ground.y - player.height;
+    player.velocityY = 0;
+    onGround = true;
+  } else {
+    OnGround = false;
+
+    onGround = false;
+
+for (let platform of platforms) {
+  const isLanding =
+    player.y + player.height >= platform.y &&
+    player.y + player.height <= platform.y + platform.height &&
+    player.x + player.width > platform.x &&
+    player.x < platform.x + platform.width &&
+    player.velocityY >= 0;
+
+  if (isLanding) {
+    player.y = platform.y - player.height;
+    player.velocityY = 0;
+    onGround = true;
+  }
+}
+  }
+}
+
+const platforms = [
+  { x: 0, y: 450, width: 800, height: 50 },
+  { x: 200, y: 350, width: 150, height: 20 },
+  { x: 400, y: 300, width: 150, height: 20 },
+  { x: 600, y: 250, width: 150, height: 20 }
+];
+
+
 
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -79,45 +122,7 @@ function gameLoop() {
 }
 gameLoop();
 
-function updatePlayer() {
-player.velocityY += gravity;
-player.y += player. velocityY;
 
 
-if (player.y + player.height >= ground.y) {
-    player.y = ground.y - player.height;
-    player.velocityY = 0;
-  }
-}
 
-const keys = {
-  left: false,
-  right: false,
-};
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "a") keys.left = true;
-  if (e.key === "d") keys.right = true;
-});
-
-document.addEventListener("keyup", (e) => {
-  if (e.key === "a") keys.left = false;
-  if (e.key === "d") keys.right = false;
-});
-
-function updatePlayer() {
-
-    if (keys.left) player.velocityX = -5;
-  if (keys.right) player.velocityX = 5;
-  else player.velocityX = 0;
-
-  player.x += player.velocityX;
-
-   player.velocityY += gravity;
-  player.y += player.velocityY;
-
-  if (player.y + player.height >= ground.y) {
-    player.y = ground.y - player.height;
-    player.velocityY = 0;
-  }
-}
