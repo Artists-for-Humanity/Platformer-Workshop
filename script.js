a21eec70022832b2c04b6d461182682c38e9a2bb
MrO3420 <omason632@gmail.com>
2026-06-11 17:48:45 -0400

    Platformer workshop
    im done with it finally.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 500;
+canvas.width = 1900;
+canvas.height = 1000;
 
 
 const player = {
@@ -37,22 +37,19 @@ const camera = {
   x: 0
 };
 const playerSprite = new Image();
-playerSprite.src = "./assets/playerGreen_roll.png";
+playerSprite.src = "/assets/playerGreen_roll.png";
 
 const groundTile = new Image();
-groundTile.src = "./assets/tileGreen_27.png";
+groundTile.src = "/assets/tileGreen_27.png";
 
 const backgroundImage = new Image();
-backgroundImage.src = "./assets/backgroundColorGrass.png";
+backgroundImage.src = "/assets/background.png";
 
 const coinSprite = new Image();
-coinSprite.src = "./assets/coin.png";
+coinSprite.src = "/assets/coin_37.png";
 
 const goalSprite = new Image();
-goalSprite.src = "./assets/flag.png";
-
-
-
+goalSprite.src = "/assets/PR.png";
 
 
 let score = 0;
@@ -78,7 +75,7 @@ const keys = {
 };
 
 document.addEventListener("keydown", (e) => {
-  console.log(e);
+  // console.log(e);
   if (e.key === "a") keys.left = true;
   if (e.key === "d") keys.right = true;
 
@@ -91,6 +88,32 @@ document.addEventListener("keyup", (e) => {
   if (e.key === "a") keys.left = false;
   if (e.key === "d") keys.right = false;
 });
+
+
+function drawGround() {
+ for (let platform of platforms) {
+  for (let x = platform.x; x < platform.x + platform.width; x += 32) {
+    ctx.drawImage(
+      groundTile,
+      x - camera.x,
+      platform.y,
+      32,
+      32
+    );
+  }
+}
+}
+
+
+function drawPlayer() {
+  ctx.drawImage(
+  playerSprite,
+  player.x - camera.x,
+  player.y,
+  player.width,
+  player.height
+);
+}
 
 
 
@@ -140,77 +163,54 @@ if (
   gameWon = true;
 }
 
-}
-
-
-
-for (let coin of coins) {
-  if(!coin.collected) {
+  for (let coin of coins) {
+  if (!coin.collected) {
     const dx = player.x + player.width / 2 - coin.x;
-    const dy = player.y + player.height /2 - coin.y;
+    const dy = player.y + player.height / 2 - coin.y;
     const distance = Math.sqrt(dx * dx + dy * dy);
-    
+
     if (distance < coin.size) {
       coin.collected = true;
       score++;
     }
   }
 }
+  
 
-function drawGround() {
- for (let platform of platforms) {
-  for (let x = platform.x; x < platform.x + platform.width; x += 32) {
-    ctx.drawImage(
-      groundTile,
-      x - camera.x,
-      platform.y,
-      32,
-      32
-    );
-  }
-}
 
 }
 
-function drawPlayer() {
-  ctx.drawImage(
-  playerSprite,
-  player.x - camera.x,
-  player.y,
-  player.width,
-  player.height
-);
-}
 
 
 function drawGoal() {
-  ctx.fillStyle = "red";
-  ctx.fillRect(goal.x - camera.x, goal.y, goal.width, goal.height);
+  ctx.drawImage(
+  goalSprite,
+  goal.x - camera.x,
+  goal.y,
+  goal.width,
+  goal.height
+);
 }
+
 
 
 function drawCoins() {
     for (let coin of coins) {
       if (!coin.collected) {
-        ctx.fillStyle = "gold";
-        ctx.beginPath();
-        ctx.arc(coin.x - camera.x, coin.y, coin.size / 2, 0, Math.PI * 2);
-        ctx.fill();
-        for (let coin of coins) {
-          if(!coin.collected) {
-            const dx = player.x + player.width / 2 - coin.x;
-            const dy = player.y + player.height /2 - coin.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-            
-            if (distance < coin.size) {
-              coin.collected = true;
-              score++;
-            }
-          }
-        }
+        ctx.drawImage(
+          coinSprite,
+          coin.x - camera.x - coin.size / 2,
+          coin.y - coin.size / 2,
+          coin.size,
+          coin.size
+        );
       }
     }
   }
+
+
+
+
 
 function gameWin(){
   if (gameWon) {
@@ -225,8 +225,25 @@ function showScore() {
   ctx.fillText("coins: "+ score, 20, 30);
 }
 
+function drawBackground() {
+ const bgWidth = canvas.width;
+
+ let offset = (-camera.x * 0.2)% bgWidth;
+
+ for (let x = offset - bgWidth; x < canvas.width; x += bgWidth) {
+  ctx.drawImage(backgroundImage, x, 0, bgWidth, canvas.height);
+ }
+}
+
+
+playerSprite.onload = () => {
+  gameLoop();
+};
+
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+    drawBackground();
 
     updatePlayer();
     drawCoins();
@@ -241,19 +258,3 @@ function gameLoop() {
 }
 gameLoop();
 
-
-for (let platform of platforms) {
-  for (let x = platform.x; x < platform.x + platform.width; x += 32) {
-    ctx.drawImage(
-      groundTile,
-      x - camera.x,
-      platform.y,
-      32,
-      32
-    );
-  }
-}
-
-playerSprite.onload = () => {
-  gameLoop();
-};
